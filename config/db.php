5bd4dcfe99b10b05d6ef7c5faafdc165f7d1f38f
maik101010 <maiksoft10@gmail.com>
2018-02-10 00:18:47 -0500

    Se agrega la conexión de heroku

--- config/db.php
+++ config/db.php
@@ -1,14 +1,31 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;port=5432;dbname=prueba',
-    'username' => 'postgres',
-    'password' => '1234',
-    'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+         'class' => 'yii\db\Connection',
+         'dsn' => 'pgsql:host=ec2-54-225-199-107.compute-1.amazonaws.com;port=5432;dbname=d3tsh3davnsg8r',
+         'username' => 'dmoyrnbrevqllf',
+         'password' => '88b6161ccfa79292c94f43974dbe2f79764b4f96ee77655047d9004668121a1d',
+         'charset' => 'utf8',
 ];
+
+// return [
+//     'class' => 'yii\db\Connection',
+//     'dsn' => 'pgsql:host=localhost;port=5432;dbname=prueba',
+//     'username' => 'postgres',
+//     'password' => '1234',
+//     'charset' => 'utf8',
+
+// //     ec2-54-225-199-107.compute-1.amazonaws.com
+// // Database
+// // d3tsh3davnsg8r
+// // User
+// // dmoyrnbrevqllf
+// // Port
+// // 5432
+// // Password
+// // 88b6161ccfa79292c94f43974dbe2f79764b4f96ee77655047d9004668121a1d
+//     // Schema cache options (for production environment)
+//     //'enableSchemaCache' => true,
+//     //'schemaCacheDuration' => 60,
+//     //'schemaCache' => 'cache',
+// ];
